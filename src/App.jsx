8b600f4e1ce73c7429c1dc3dd7d052adff940d3b
Eslam Mohamed <eslama202002@gmail.com>
2026-04-21 21:58:13 +0200

    connecting to web 3 forms

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Services from './compnents/Services';
 import OurLatesetWork from './compnents/OurLatesetWork';
 import Team from './compnents/Team';
 import ContactUs from './compnents/ContactUs';
-
+import { Toaster } from 'react-hot-toast';
 function App() {
 const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   return (
     <div className='dark:bg-gray-900'>
+      <Toaster/>
      <Navbar theme={theme} setTheme={setTheme}/>
      <Hero></Hero>
      <TrustedBy/>
